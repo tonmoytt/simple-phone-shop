a732ab452e4635f1ca5a97a5e608f3ee30a53445
Tafsirul Tonmoy <tonmoytafsirul9@gmail.com>
2025-08-22 01:56:32 +0600

    add and polish all

--- src/Components/Component/Home/Home.jsx
+++ src/Components/Component/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Phones from '../Phones/Phones';
 import Brands from '../Brands/Brands';
+import AnotherPhones from '../AnotherPhones/AnotherPhones';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
            
            <Banner></Banner> 
            <Brands></Brands>
+           <AnotherPhones></AnotherPhones>
            <Phones></Phones>
         </div>
     );
